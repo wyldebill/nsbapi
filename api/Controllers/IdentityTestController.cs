0b6b09568ebc37a42af4916c526043a8b8b2b781
William Austin <waustin@problemsolutions.net>
2019-06-27 08:59:12 -0500

    remove authentication api, add ocelot gateway.

--- api/Controllers/IdentityTestController.cs
+++ api/Controllers/IdentityTestController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
 {
-   [Authorize]
+   //[Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class IdentityTestController : ControllerBase
